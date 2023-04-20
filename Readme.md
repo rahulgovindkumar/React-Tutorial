333a84956cbebb8e9e98745635461bf0a798e1a1
rahulgovindkumar <rahulgovindkumar@gmail.com>
2023-04-20 14:10:01 -0400

    react example tutorial

---
+++ Readme.md
@@ -0,0 +1,14 @@
+npm create vite@4.1.0
+ - Choose react
+  - TypeScript
+
+ cd react-app
+  npm install
+  npm run dev
+
+
+Start Dev Server:
+npm run dev
+
+Install Bootstarp
+npm i bootstrap@5.2.3
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  const items = ["New york", "London", "Paris", "Tokyo", "Moscow"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
